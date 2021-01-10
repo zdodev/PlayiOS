246439798949a4154c36856f365254dbd56496f6
zdodev <iver0945@gmail.com>
2021-01-10 18:24:56 +0900

    🧗: InsertSubview practice

--- UIDevelopmenProgrammatically/UIDevelopmenProgrammatically/ViewController.swift
+++ UIDevelopmenProgrammatically/UIDevelopmenProgrammatically/ViewController.swift
@@ -8,16 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-    var navigationButton: UIButton!
-    var emailTextField: UITextField!
-    var updateSwitch: UISwitch!
-    var intervalStepper: UIStepper!
-    var intervalValueLabel: UILabel!
-    var updateSwitchLabel: UILabel!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemGray4
         
+        let rect1 = CGRect(x: 10, y: 100, width: 100, height: 100)
+        let firstView = UIView(frame: rect1)
+        firstView.backgroundColor = .cyan
+        view.addSubview(firstView)
+        
+        let rect2 = CGRect(x: 10, y: 10, width: 150, height: 150)
+        let secondView = UIView(frame: rect2)
+        secondView.backgroundColor = .black
+//        firstView.addSubview(secondView)
+        view.insertSubview(secondView, aboveSubview: firstView)
+        
+        print("ee")
     }
 }
