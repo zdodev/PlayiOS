a130edb92b723b8e1b4600a7550b39560a538be3
zdodev <iver0945@gmail.com>
2021-01-17 22:00:22 +0900

    ✅: Call application(_:didFinishLaunchingWithOptions:).

--- iOSAppLifeCycle/iOSAppLifeCycle/AppDelegate.swift
+++ iOSAppLifeCycle/iOSAppLifeCycle/AppDelegate.swift
@@ -24,8 +24,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return true
     }
 
+    // 4. application(_:didFinishLaunchingWithOptions:)
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        print("call... application(_:didFinishLaunchingWithOptions:)")
         return true
     }
 
