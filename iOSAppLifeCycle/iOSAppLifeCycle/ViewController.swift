89bd2c62b5421cbdd09629ec1723f76259f33e43
zdodev <iver0945@gmail.com>
2021-01-17 21:23:41 +0900

    🎉: Begin iOSAppLifeCycle project.

---
+++ iOSAppLifeCycle/iOSAppLifeCycle/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOSAppLifeCycle
+//
+//  Created by Zero DotOne on 2021/01/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
