8814301382284a51ebb839be3f2b9896af16068d
zdodev <iver0945@gmail.com>
2021-01-17 22:08:18 +0900

    ✅: Call sceneDidEnterBackground(_:).

--- iOSAppLifeCycle/iOSAppLifeCycle/SceneDelegate.swift
+++ iOSAppLifeCycle/iOSAppLifeCycle/SceneDelegate.swift
@@ -40,6 +40,14 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This may occur due to temporary interruptions (ex. an incoming phone call).
         print("call... sceneWillResignActive(_:)")
     }
+    
+    // 9. sceneDidEnterBackground(_:)
+    func sceneDidEnterBackground(_ scene: UIScene) {
+        // Called as the scene transitions from the foreground to the background.
+        // Use this method to save data, release shared resources, and store enough scene-specific state information
+        // to restore the scene back to its current state.
+        print("call... sceneDidEnterBackground(_:)")
+    }
 
     func sceneDidDisconnect(_ scene: UIScene) {
         // Called as the scene is being released by the system.
@@ -47,13 +55,5 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Release any resources associated with this scene that can be re-created the next time the scene connects.
         // The scene may re-connect later, as its session was not necessarily discarded (see `application:didDiscardSceneSessions` instead).
     }
-
-    func sceneDidEnterBackground(_ scene: UIScene) {
-        // Called as the scene transitions from the foreground to the background.
-        // Use this method to save data, release shared resources, and store enough scene-specific state information
-        // to restore the scene back to its current state.
-    }
-
-
 }
 
