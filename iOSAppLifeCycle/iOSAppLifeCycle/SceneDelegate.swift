3c81e631ceaddd94958132eeacc005319dff488c
zdodev <iver0945@gmail.com>
2021-01-17 22:05:15 +0900

    ✅: Call sceneWillEnterForeground(_:).

--- iOSAppLifeCycle/iOSAppLifeCycle/SceneDelegate.swift
+++ iOSAppLifeCycle/iOSAppLifeCycle/SceneDelegate.swift
@@ -20,6 +20,13 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let _ = (scene as? UIWindowScene) else { return }
     }
 
+    // 6. sceneWillEnterForeground(_:)
+    func sceneWillEnterForeground(_ scene: UIScene) {
+        // Called as the scene transitions from the background to the foreground.
+        // Use this method to undo the changes made on entering the background.
+        print("call... sceneWillEnterForeground(_:)")
+    }
+
     func sceneDidDisconnect(_ scene: UIScene) {
         // Called as the scene is being released by the system.
         // This occurs shortly after the scene enters the background, or when its session is discarded.
@@ -35,11 +42,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func sceneWillResignActive(_ scene: UIScene) {
         // Called when the scene will move from an active state to an inactive state.
         // This may occur due to temporary interruptions (ex. an incoming phone call).
-    }
-
-    func sceneWillEnterForeground(_ scene: UIScene) {
-        // Called as the scene transitions from the background to the foreground.
-        // Use this method to undo the changes made on entering the background.
     }
 
     func sceneDidEnterBackground(_ scene: UIScene) {
