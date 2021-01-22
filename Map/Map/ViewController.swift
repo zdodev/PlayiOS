bb49d17fc727a22c6f80c6638d918ed0a125c0e5
zdodev <iver0945@gmail.com>
2021-01-22 20:48:39 +0900

    🗺: Setting map.

--- Map/Map/ViewController.swift
+++ Map/Map/ViewController.swift
@@ -6,14 +6,20 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var myMap: MKMapView!
+    @IBOutlet weak var lblLocationInfo1: UILabel!
+    @IBOutlet weak var lblLocationInfo2: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func sgChangeLocation(_ sender: UISegmentedControl) {
+    }
+    
 }
 
