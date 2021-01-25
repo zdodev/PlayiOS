f41410907052f3d7a92c71bbaf1c8e93314f71f9
zdodev <iver0945@gmail.com>
2021-01-25 13:12:53 +0900

    🎉: CollectionView-2.

---
+++ CollectionView-2/CollectionView-2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CollectionView-2
+//
+//  Created by Zero DotOne on 2021/01/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
