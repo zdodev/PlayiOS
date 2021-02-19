c378b68bfb709a3e243f0f9f4fcdc1d56fe9cc12
zdodev <iver0945@gmail.com>
2021-02-20 02:33:36 +0900

    🎉: Begin splitViewController-1 project.

---
+++ SplitViewController/SplitViewController-1/SplitViewController-1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SplitViewController-1
+//
+//  Created by Zero DotOne on 2021/02/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
