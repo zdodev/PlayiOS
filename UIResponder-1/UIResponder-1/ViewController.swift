eb6977549c068ef2ea3df218ea9a5859cee7827c
zdodev <iver0945@gmail.com>
2021-02-22 13:29:41 +0900

    🔥: 불필요한 코드 제거.

--- UIResponder-1/UIResponder-1/ViewController.swift
+++ UIResponder-1/UIResponder-1/ViewController.swift
@@ -1,19 +1,4 @@
-//
-//  ViewController.swift
-//  UIResponder-1
-//
-//  Created by Zero DotOne on 2021/02/22.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
 }
-
