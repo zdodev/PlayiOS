e35c3b41c769dcb7a215261ac042896ed3ca9367
zdodev <iver0945@gmail.com>
2021-02-22 14:40:27 +0900

    ✨: Responder chain 확인.

--- UIResponder-1/UIResponder-1/ViewController.swift
+++ UIResponder-1/UIResponder-1/ViewController.swift
@@ -1,4 +1,30 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let view1: UIView = {
+        let view = UIView(frame: CGRect(x: 0, y: 50, width: 100, height: 100))
+        view.backgroundColor = .systemRed
+        return view
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemBlue
+        
+        view.addSubview(view1)
+        
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        var responder = view1.next
+        
+        // 최하위 뷰의 next responder
+        print(responder!)
+        while let nextResponder = responder?.next {
+            print(nextResponder)
+            responder = nextResponder
+        }
+    }
 }
