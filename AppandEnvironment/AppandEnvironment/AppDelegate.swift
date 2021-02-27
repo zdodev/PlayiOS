eef8ada0cdff71f9fd4a08dd7d705043905660e3
zdodev <iver0945@gmail.com>
2021-02-27 12:19:30 +0900

    🏙: Scene lifecycle 구현.

--- AppandEnvironment/AppandEnvironment/AppDelegate.swift
+++ AppandEnvironment/AppandEnvironment/AppDelegate.swift
@@ -2,6 +2,15 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    // MARK: main() 타입 메서드 구현
+    // UIApplicationDelegate에 명시되어 있는 main() 메서드 명시적으로 구현.
+    static func main() {
+        print("call... main()")
+        print("call... UIApplication(_:_:_:_:)")
+        // UIApplicationMain(_:_:_:_:) 함수 호출
+        UIApplicationMain(CommandLine.argc, CommandLine.unsafeArgv, nil, NSStringFromClass(self))
+    }
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
--- AppandEnvironment/AppandEnvironment/ViewController.swift
+++ AppandEnvironment/AppandEnvironment/ViewController.swift
@@ -6,6 +6,10 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         view.backgroundColor = .white
+        let subView = UIView()
+        subView.frame = CGRect(x: 100, y: 100, width: 100, height: 100)
+        subView.backgroundColor = .systemTeal
+        view.addSubview(subView)
     }
 }
 
