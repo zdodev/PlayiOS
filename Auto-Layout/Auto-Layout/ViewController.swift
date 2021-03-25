197ecd5d45a6fae981345ed329bf9b89562c9cc9
zdodev <iver0945@gmail.com>
2021-03-26 00:00:54 +0900

    🎨: Label 작성.

--- Auto-Layout/Auto-Layout/ViewController.swift
+++ Auto-Layout/Auto-Layout/ViewController.swift
@@ -2,6 +2,7 @@ import UIKit
 
 class ViewController: UIViewController {
     let button = UIButton()
+    let label = UILabel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -11,13 +12,25 @@ class ViewController: UIViewController {
         button.setTitle("Button", for: .normal)
         button.setTitleColor(.systemRed, for: .normal)
         button.backgroundColor = .systemTeal
+        
         button.frame = CGRect(x: 0, y: 0, width: 200, height: 200)
         button.translatesAutoresizingMaskIntoConstraints = false
-        button.center = view.center
         button.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         button.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
         button.heightAnchor.constraint(equalToConstant: 200).isActive = true
         button.widthAnchor.constraint(equalToConstant: 200).isActive = true
+        
+        print(button.frame)
+        print(button.bounds)
+        
+        label.text = "Label"
+        label.backgroundColor = .cyan
+        view.addSubview(label)
+
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.topAnchor.constraint(equalTo: button.bottomAnchor, constant: 30).isActive = true
+        label.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30).isActive = true
+        label.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30).isActive = true
     }
 }
 
--- Auto-Layout/README.md
+++ Auto-Layout/README.md
@@ -1,6 +1,14 @@
 # Auto Layout
 
-- Auto layout은 뷰 요소의 위치 좌표(x, y)와 크기 좌표(width, height) 4가지가 정해져야 auto layout을 지정할 수 있습니다.
+- Auto layout은 뷰 요소의 위치 좌표(x, y)와 크기(width, height)가 정해져야 auto layout을 지정할 수 있습니다.
+- x
+- y
+- width(horizontal)
+- 
+
+### UI를 스토리보드가 아닌 코드로 UI를 작성하고 auto layout을 설정하는 이유는?
+
+스토리보드의 경우 협업 시 코드 충돌이 빈번하게 발생하고, 충돌을 해결하기 쉽지 않기 때문에 뷰의 위치와 크기를 명시적으로 작성한 코드를 통해 작성하는 경우가 많습니다.
 
 ## 참고 URL
 
