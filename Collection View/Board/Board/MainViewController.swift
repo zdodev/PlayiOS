270e42e2429410ca33f1a100df1689e5eb57d73b
zdodev <iver0945@gmail.com>
2021-04-01 00:11:51 +0900

    🎨: 단일 collection view 구현 및 셀 출력.

--- Collection View/Board/Board/MainViewController.swift
+++ Collection View/Board/Board/MainViewController.swift
@@ -1,16 +1,52 @@
 import UIKit
 
 class MainViewController: UIViewController {
+    let todoCollectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .systemBackground
         setupNavigationBar()
+        setupMemoCollectionView()
     }
     
     private func setupNavigationBar() {
         navigationItem.title = "Project Manager"
         navigationItem.setRightBarButton(UIBarButtonItem(systemItem: .add), animated: true)
     }
+    
+    private func setupMemoCollectionView() {
+        view.addSubview(todoCollectionView)
+        
+        todoCollectionView.translatesAutoresizingMaskIntoConstraints = false
+        let safeArea = view.safeAreaLayoutGuide
+        NSLayoutConstraint.activate([
+            todoCollectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            todoCollectionView.widthAnchor.constraint(equalToConstant: view.frame.width / 3),
+            todoCollectionView.topAnchor.constraint(equalTo: safeArea.topAnchor),
+            todoCollectionView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor)
+        ])
+        
+        todoCollectionView.register(MemoCollectionViewCell.self, forCellWithReuseIdentifier: MemoCollectionViewCell.identifier)
+        todoCollectionView.dataSource = self
+        todoCollectionView.delegate = self
+    }
 }
 
+extension MainViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        30
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = todoCollectionView.dequeueReusableCell(withReuseIdentifier: MemoCollectionViewCell.identifier, for: indexPath) as! MemoCollectionViewCell
+        return cell
+    }
+}
+
+extension MainViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        CGSize(width: 200, height: 200)
+    }
+}
