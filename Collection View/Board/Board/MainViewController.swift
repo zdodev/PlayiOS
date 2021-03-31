91adde43499d0fddedabb80d2b67dcd5fc60c495
zdodev <iver0945@gmail.com>
2021-03-31 21:39:29 +0900

    🪐: navigation bar title 및 rightButton 작성.

--- Collection View/Board/Board/MainViewController.swift
+++ Collection View/Board/Board/MainViewController.swift
@@ -1,14 +1,16 @@
 import UIKit
 
 class MainViewController: UIViewController {
-    let uiview = UIView()
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
-        uiview.frame = CGRect(x: 50, y: 50, width: 50, height: 50)
-        uiview.backgroundColor = .black
-        view.addSubview(uiview)
+        
+        view.backgroundColor = .systemBackground
+        setupNavigationBar()
+    }
+    
+    private func setupNavigationBar() {
+        navigationItem.title = "Project Manager"
+        navigationItem.setRightBarButton(UIBarButtonItem(systemItem: .add), animated: true)
     }
 }
 
