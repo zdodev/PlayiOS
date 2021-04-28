31d050a7032eb17ad183bbccf875fcca8a93bda9
zdodev <iver0945@gmail.com>
2021-04-29 00:36:19 +0900

    ✨: 커넥션 담당 객체 생성.

---
+++ SocketNetworking/SocketNetworking/StreamSession.swift
@@ -0,0 +1,33 @@
+import Foundation
+
+struct StreamConnection {
+    private let serverAddress: CFString
+    private let serverPort: UInt32
+    private var inputStream: InputStream!
+    private let outputStream: OutputStream!
+    
+    init(address: String, port: UInt32) {
+        serverAddress = address as CFString
+        serverPort = port
+        
+        var readStream: Unmanaged<CFReadStream>!
+        var writeStream: Unmanaged<CFWriteStream>!
+        
+        CFStreamCreatePairWithSocketToHost(kCFAllocatorDefault, serverAddress, serverPort, &readStream, &writeStream)
+        
+        inputStream = readStream.takeRetainedValue()
+        outputStream = writeStream.takeRetainedValue()
+    }
+    
+    func inputStreamConnection() -> InputStream {
+        inputStream.schedule(in: .current, forMode: .common)
+        
+        return inputStream
+    }
+
+    func outputStreamConnection() -> OutputStream {
+        outputStream.schedule(in: .current, forMode: .common)
+        
+        return outputStream
+    }
+}
