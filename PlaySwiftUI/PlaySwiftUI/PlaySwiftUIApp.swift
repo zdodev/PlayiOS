8e1f49e2206d052f6e122f4e2db3cb14d44f6ca9
zdodev <iver0945@gmail.com>
2021-07-03 22:27:41 +0900

    🎉: SwiftUI 프로젝트 시작.

---
+++ PlaySwiftUI/PlaySwiftUI/PlaySwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlaySwiftUIApp.swift
+//  PlaySwiftUI
+//
+//  Created by Zero DotOne on 2021/07/03.
+//
+
+import SwiftUI
+
+@main
+struct PlaySwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
