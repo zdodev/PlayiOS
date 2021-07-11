053a3421163e3953f1224e8f61f8ebffe6e615eb
zdodev <iver0945@gmail.com>
2021-07-11 23:51:48 +0900

    📱: SwiftUI text, padding, background.

--- PlaySwiftUI/PlaySwiftUI/Views/ContentView.swift
+++ PlaySwiftUI/PlaySwiftUI/Views/ContentView.swift
@@ -2,58 +2,14 @@ import SwiftUI
 
 // View 프로토콜을 채택
 struct ContentView: View {
-    let carStack = HStack {
-        Text("Car Image")
-        Image(systemName: "car.fill")
-    }
-    
     var body: some View {
-        Button(action:
-            tappedButton, label: {
-            Text("Button")
-        })
-    }
-    
-    private func tappedButton() {
-        print("Button")
-    }
-}
-
-struct MyHStackView: View {
-    var body: some View {
-        HStack {
-            Text("myHStackView 1")
-            Text("myHStackView 2")
+        VStack {
+            MyVStack()
+            MyVStack()
+            MyVStack()
         }
-    }
-}
-
-struct StandardTitle: ViewModifier {
-    func body(content: Content) -> some View {
-        content
-            .font(.largeTitle)
-            .background(Color.white)
-            .border(Color.gray, width: 0.2)
-            .shadow(color: .black, radius: 5, x: 0, y: 5)
-    }
-}
-
-struct BorderedCaption: ViewModifier {
-    func body(content: Content) -> some View {
-        content
-            .font(.caption2)
-            .padding(10)
-            .overlay(
-                RoundedRectangle(cornerRadius: 15)
-                    .stroke(lineWidth: 1)
-            )
-            .foregroundColor(.blue)
-    }
-}
-
-extension View {
-    func borderedCaption() -> some View {
-        modifier(BorderedCaption())
+        .padding(.horizontal, 30)
+        .background(Color.yellow)
     }
 }
 
---
+++ PlaySwiftUI/PlaySwiftUI/Views/MyVStack.swift
@@ -0,0 +1,24 @@
+import SwiftUI
+
+struct MyVStack: View {
+    var body: some View {
+        HStack {
+            Text("1!")
+                .fontWeight(.bold)
+                .font(.system(size: 80))
+            Text("2!")
+                .fontWeight(.bold)
+                .font(.system(size: 80))
+            Text("3!")
+                .fontWeight(.bold)
+                .font(.system(size: 80))
+        }
+        .background(Color.green)
+    }
+}
+
+struct MyVStack_Previews: PreviewProvider {
+    static var previews: some View {
+        MyVStack()
+    }
+}
