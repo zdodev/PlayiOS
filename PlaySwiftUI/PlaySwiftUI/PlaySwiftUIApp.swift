0e6cb934febfea28988ceb1e766ea6e4f53b2522
zdodev <iver0945@gmail.com>
2021-07-11 22:25:22 +0900

    📱: SwiftUI 정리.

--- PlaySwiftUI/PlaySwiftUI/PlaySwiftUIApp.swift
+++ PlaySwiftUI/PlaySwiftUI/PlaySwiftUIApp.swift
@@ -1,7 +1,10 @@
 import SwiftUI
 
+// App, 앱의 구조와 동작을 나타내는 프로토콜
+// 앱의 초기 진입점
 @main
 struct PlaySwiftUIApp: App {
+    // body 프로퍼티 구현
     var body: some Scene {
         WindowGroup {
             ContentView()
--- PlaySwiftUI/PlaySwiftUI/Views/ContentView.swift
+++ PlaySwiftUI/PlaySwiftUI/Views/ContentView.swift
@@ -1,11 +1,63 @@
 import SwiftUI
 
+// View 프로토콜을 채택
 struct ContentView: View {
+    let carStack = HStack {
+        Text("Car Image")
+        Image(systemName: "car.fill")
+    }
+    
     var body: some View {
-        LandmarkList()
+        Button(action:
+            tappedButton, label: {
+            Text("Button")
+        })
+    }
+    
+    private func tappedButton() {
+        print("Button")
     }
 }
 
+struct MyHStackView: View {
+    var body: some View {
+        HStack {
+            Text("myHStackView 1")
+            Text("myHStackView 2")
+        }
+    }
+}
+
+struct StandardTitle: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.largeTitle)
+            .background(Color.white)
+            .border(Color.gray, width: 0.2)
+            .shadow(color: .black, radius: 5, x: 0, y: 5)
+    }
+}
+
+struct BorderedCaption: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.caption2)
+            .padding(10)
+            .overlay(
+                RoundedRectangle(cornerRadius: 15)
+                    .stroke(lineWidth: 1)
+            )
+            .foregroundColor(.blue)
+    }
+}
+
+extension View {
+    func borderedCaption() -> some View {
+        modifier(BorderedCaption())
+    }
+}
+
+// PreviewProvider 프로토콜을 채택
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
