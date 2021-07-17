472a000b53cacbc715b95a7ec280b1fd4976c5f9
zdodev <iver0945@gmail.com>
2021-07-17 09:37:34 +0900

    📱: SwiftUI.

--- PlaySwiftUI/PlaySwiftUI/Views/ContentView.swift
+++ PlaySwiftUI/PlaySwiftUI/Views/ContentView.swift
@@ -2,7 +2,29 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        CircleImageView()
+        VStack(alignment: .trailing, spacing: 50) {
+//            Rectangle()
+//                .frame(height: 0)
+            Text("글자")
+                .font(.system(size: 30))
+                .fontWeight(.heavy)
+            
+            Rectangle()
+                .frame(width: 100, height: 100)
+                .foregroundColor(.red)
+            
+            Rectangle()
+                .frame(width: 100, height: 100)
+                .foregroundColor(.orange)
+            
+            Spacer()
+            
+            Rectangle()
+                .frame(width: 100, height: 100)
+                .foregroundColor(.blue)
+        }
+        .frame(width: 300)
+        .background(Color.green)
     }
 }
 
---
+++ PlaySwiftUI/PlaySwiftUI/Views/MyVStack.swift
@@ -0,0 +1,15 @@
+import SwiftUI
+
+struct MyVStack: View {
+    var body: some View {
+        ZStack {
+            Rectangle()
+        }
+    }
+}
+
+struct MyVStack_Previews: PreviewProvider {
+    static var previews: some View {
+        MyVStack()
+    }
+}
