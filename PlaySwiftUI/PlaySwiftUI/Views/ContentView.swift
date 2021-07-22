b85a2d0bad0369538442222593596809d726f3d0
zdodev <iver0945@gmail.com>
2021-07-23 00:37:59 +0900

    🎨: SwiftUI 테이블 뷰.

--- PlaySwiftUI/PlaySwiftUI/Views/ContentView.swift
+++ PlaySwiftUI/PlaySwiftUI/Views/ContentView.swift
@@ -1,43 +1,57 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var isNavigationBarHidden = false
+    
     var body: some View {
-        ZStack(alignment: .bottomTrailing) {
-            VStack(alignment: .leading, spacing: 0) {
-                HStack {
-                    Image(systemName: "line.horizontal.3")
-                        .font(.largeTitle)
-                    Spacer()
-                    Image(systemName: "person.crop.circle.fill")
-                        .font(.largeTitle)
-                }
-                .padding(20)
-                Text("zdo 할일목록")
-                    .font(.system(size: 40))
-                    .fontWeight(.black)
-                    .padding(.horizontal, 20)
-                    .padding(.top, 20)
-                ScrollView {
-                    VStack {
-                        MyProjectCard()
-                        MyBasicCard()
-                        MyCard(icon: "tray.fill", title: "정리하기", start: "10시", end: "11시", backgroundColor: .blue)
-                        MyCard(icon: "tray.fill", title: "정리하기", start: "10시", end: "11시", backgroundColor: .secondary)
-                        MyCard(icon: "tray.fill", title: "정리하기", start: "10시", end: "11시", backgroundColor: .black)
+        NavigationView {
+            ZStack(alignment: .bottomTrailing) {
+                VStack(alignment: .leading, spacing: 0) {
+                    HStack {
+                        NavigationLink(
+                            destination: MyList(isNavigationBarHidden: $isNavigationBarHidden),
+                            label: {
+                                Image(systemName: "line.horizontal.3")
+                                    .font(.largeTitle)
+                                    .foregroundColor(.black)
+                            })
+                        Spacer()
+                        Image(systemName: "person.crop.circle.fill")
+                            .font(.largeTitle)
                     }
-                    .padding()
+                    .padding(20)
+                    Text("zdo 할일목록")
+                        .font(.system(size: 40))
+                        .fontWeight(.black)
+                        .padding(.horizontal, 20)
+                        .padding(.top, 20)
+                    ScrollView {
+                        VStack {
+                            MyProjectCard()
+                            MyBasicCard()
+                            MyCard(icon: "tray.fill", title: "정리하기", start: "10시", end: "11시", backgroundColor: .blue)
+                            MyCard(icon: "tray.fill", title: "정리하기", start: "10시", end: "11시", backgroundColor: .secondary)
+                            MyCard(icon: "tray.fill", title: "정리하기", start: "10시", end: "11시", backgroundColor: .black)
+                        }
+                        .padding()
+                    }
                 }
+                Circle()
+                    .foregroundColor(.yellow)
+                    .frame(width: 60, height: 60)
+                    .overlay(
+                        Image(systemName: "plus")
+                            .font(.system(size: 30))
+                            .foregroundColor(.white)
+                    )
+                    .padding(10)
+                    .shadow(radius: 20)
             }
-            Circle()
-                .foregroundColor(.yellow)
-                .frame(width: 60, height: 60)
-                .overlay(
-                    Image(systemName: "plus")
-                        .font(.system(size: 30))
-                        .foregroundColor(.white)
-                )
-                .padding(10)
-                .shadow(radius: 20)
+            .navigationBarTitle("뒤로가기")
+            .navigationBarHidden(isNavigationBarHidden)
+            .onAppear() {
+                isNavigationBarHidden = true
+            }
         }
     }
 }
