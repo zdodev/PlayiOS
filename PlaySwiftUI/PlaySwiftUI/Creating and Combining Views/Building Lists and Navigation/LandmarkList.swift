e7d84de9d0a5e4078c62eba8465c9174b3fd893c
zdodev <iver0945@gmail.com>
2021-09-01 15:24:59 +0900

    📱: SwiftUI tutorial.

---
+++ PlaySwiftUI/PlaySwiftUI/Creating and Combining Views/Building Lists and Navigation/LandmarkList.swift
@@ -0,0 +1,16 @@
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List {
+            LandmarkRow(landmark: landmarks[0])
+            LandmarkRow(landmark: landmarks[1])
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
