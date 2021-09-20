aa455f0569b59e0ef2fd06b9ef491324e54da944
zdodev <iver0945@gmail.com>
2021-09-21 00:41:23 +0900

    ⚡️: RxSwift

---
+++ ReactiveXiOS/README.md
@@ -0,0 +1,20 @@
+# Reactive Programming
+
+## 동기와 비동기
+
+보통의 코드는 동기로 동작하지만 비동기로 처리하기 위해서 `DispatchQueue`의 Async를 사용할 수 있다.
+
+## RxSwift를 사용하는 이유?
+
+비동기적인 작업의 코드를 조금 더 간결하게 사용하고 싶을 때
+
+## Scheduler
+
+스트림(operator)을 실행시킬 스케줄러를 결정합니다.
+
+- observe(on:) - 이후 operator를 해당 스케줄러에서 실행시킵니다.
+- subscribe(on:) - 초기 실행 스케줄러를 결정합니다.
+
+## Subject
+
+데이터를 가지고 있을 수도, 데이터를 입력해 줄 수도 있는 요소입니다.
--- ReactiveXiOS/ReactiveXiOS/ViewController2.swift
+++ ReactiveXiOS/ReactiveXiOS/ViewController2.swift
@@ -2,9 +2,11 @@ import UIKit
 import RxSwift
 
 class ViewController2: UIViewController {
+    @IBOutlet weak var imageView: UIImageView!
+    
     var disposeBag = DisposeBag()
     
-    @IBAction func exJust1(_ sender: UIButton) {
+    @IBAction func exJust1(_ sender: UIButton)  {
         Observable.just("Hello world")
             .subscribe { event in
                 switch event {
@@ -19,7 +21,7 @@ class ViewController2: UIViewController {
             .disposed(by: disposeBag)
     }
     
-    @IBAction func exJust2(_ sender: UIButton) {
+    @IBAction func exJust2(_ sender: UIButton)  {
         Observable.just(["Hello", "World"])
             .subscribe(onNext: { str in
                 print(str)
@@ -27,7 +29,7 @@ class ViewController2: UIViewController {
             .disposed(by: disposeBag)
     }
     
-    @IBAction func exFrom1(_ sender: UIButton) {
+    @IBAction func exFrom1(_ sender: UIButton)  {
         Observable.from(["RxSwift", "In", "4", "Hours"])
             .subscribe(onNext: { element in
                 print(element)
@@ -68,6 +70,24 @@ class ViewController2: UIViewController {
             .disposed(by: disposeBag)
     }
     
-    
-    
+    @IBAction func exMap3(_ sender: UIButton) {
+        Observable.just("800x600")
+            .subscribe(on: ConcurrentDispatchQueueScheduler(qos: .default))
+            .observe(on: ConcurrentDispatchQueueScheduler(qos: .default))
+            .map { $0.replacingOccurrences(of: "x", with: "/") }
+            .map { "https://picsum.photos/\($0)/?random" }
+            .map { URL(string: $0) }
+            .filter { $0 != nil }
+            .map { $0! }
+            .map { try Data(contentsOf: $0) }
+            .map { UIImage(data:  $0) }
+            .do(onNext: { image in
+                print(image?.size)
+            })
+            .observe(on: MainScheduler.instance)
+            .subscribe(onNext: { image in
+                self.imageView.image = image
+            })
+            .disposed(by: disposeBag)
+    }
 }
