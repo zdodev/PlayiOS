f8f64d405ac0f0873f7d250b8c8293ebd7c004d9
zdodev <iver0945@gmail.com>
2021-09-29 17:29:20 +0900

    ✨: Combine custom subscriber

--- ReactiveXiOS/ReactiveXiOS/ViewController3.swift
+++ ReactiveXiOS/ReactiveXiOS/ViewController3.swift
@@ -1,4 +1,5 @@
 import UIKit
+import Combine
 import RxSwift
 
 class ViewController3:
@@ -13,7 +14,7 @@ class ViewController3:
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
     }
     
     @IBAction func tappedObservable(_ sender: UIButton) {
@@ -43,5 +44,47 @@ class ViewController3:
     
     @IBAction func tappedOnNext(_ sender: UIButton) {
         subject.onNext(1)
+    }
+    
+    var anyCancellable = Set<AnyCancellable>()
+    
+    @IBAction func tappedJust(_ sender: UIButton) {
+//        let publisher = Just(1)
+//        publisher
+//            .sink { element in
+//                print(element)
+//            }
+//            .store(in: &anyCancellable)
+        
+//        let publisher = (1...10).publisher
+//        publisher.sink { _ in
+//            print("complete")
+//        } receiveValue: { element in
+//            print(element)
+//        }
+//        .store(in: &anyCancellable)
+        
+        let publisher = ["A", "B", "C"].publisher
+        let subscriber1 = CustomSubscriber()
+        publisher
+            .subscribe(subscriber1)
+    }
+    
+    
+}
+
+class CustomSubscriber: Subscriber {
+    func receive(subscription: Subscription) {
+        print("데이터의 구독을 시작합니다.")
+        subscription.request(.unlimited)
+    }
+    
+    func receive(_ input: String) -> Subscribers.Demand {
+        print("데이터를 받았습니다.", input)
+        return .none
+    }
+    
+    func receive(completion: Subscribers.Completion<Never>) {
+        print("모든 데이터의 발행이 완료되었습니다.")
     }
 }
