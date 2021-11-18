9f22ef6788312f99da281b703c04945a2b09ee2d
zdodev <iver0945@gmail.com>
2021-11-19 01:56:31 +0900

    📱: 의자 뷰 구현

--- PlaySwiftUI/Furniture-Shop-App-UI-SwiftUI-Starting_Project/Furniture_app/ContentView.swift
+++ PlaySwiftUI/Furniture-Shop-App-UI-SwiftUI-Starting_Project/Furniture_app/ContentView.swift
@@ -1,16 +1,40 @@
-//
-//  ContentView.swift
-//  Furniture_app
-//
-//  Created by Abu Anwar MD Abdullah on 14/2/21.
-//
-
 import SwiftUI
+
+// HomeScreen()
 
 struct ContentView: View {
     var body: some View {
         VStack {
-            HomeScreen()
+            ZStack {
+                Color("Bg")
+                    .edgesIgnoringSafeArea(.all)
+                
+                VStack {
+                    Image("chair_1")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .edgesIgnoringSafeArea(.top)
+
+                    VStack(alignment: .leading) {
+                        Text("Luruxy Swedia \nChair")
+                            .font(.title)
+                            .fontWeight(.bold)
+                        
+                        HStack(spacing: 4) {
+                            ForEach(0 ..< 5) { item in
+                                Image("star")
+                            }
+                            Text("(4.9)")
+                                .opacity(0.5)
+                                .padding(.leading, 8)
+                            Spacer()
+                        }
+                    }
+                    .padding()
+                    .background(Color("Bg"))
+                    .cornerRadius(40.0)
+                }
+            }
         }
     }
 }
@@ -20,5 +44,3 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
-
-
