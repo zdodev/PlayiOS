7eea949264c867d8187b9f2d07f0e911485bac1a
zdodev <iver0945@gmail.com>
2021-12-24 14:37:37 +0900

    🔔: 원격 알람 등록.

--- UserNotifications/UserNotificationTutorial/UserNotificationTutorial/AppDelegate.swift
+++ UserNotifications/UserNotificationTutorial/UserNotificationTutorial/AppDelegate.swift
@@ -6,14 +6,24 @@
 //
 
 import UIKit
+// Firebase 모듈 추가
+import Firebase
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        // Firebase 초기화
+        FirebaseApp.configure()
+        
+        // 원격 알림 등록
+        UNUserNotificationCenter.current().delegate = self
+        let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+        UNUserNotificationCenter.current().requestAuthorization(
+            options: authOptions) { granted, error in
+                print(granted)
+            }
+        application.registerForRemoteNotifications()
+        
         return true
     }
 
@@ -30,7 +40,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
 }
 
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    
+}
