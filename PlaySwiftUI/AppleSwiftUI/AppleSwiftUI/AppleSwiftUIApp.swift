792f20810d23927044750f37cbe33c1a5d8e0542
zdodev <iver0945@gmail.com>
2022-01-07 14:15:55 +0900

    🎉: AppleSwiftUI 프로젝트 생성

---
+++ PlaySwiftUI/AppleSwiftUI/AppleSwiftUI/AppleSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleSwiftUIApp.swift
+//  AppleSwiftUI
+//
+//  Created by admin on 2022/01/07.
+//
+
+import SwiftUI
+
+@main
+struct AppleSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PlaySwiftUI/AppleSwiftUI/AppleSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AppleSwiftUI
+//
+//  Created by admin on 2022/01/07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
