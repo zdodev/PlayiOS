ee476f9586b3a81fe212bc079aad2f7cb071562b
zdodev <iver0945@gmail.com>
2022-09-07 10:47:05 +0900

    ✨: UIView frame 출력.

--- LifeCycle/LifeCycle/MainView.swift
+++ LifeCycle/LifeCycle/MainView.swift
@@ -7,37 +7,37 @@ final class MainView: UIView {
     override func didAddSubview(_ subview: UIView) {
         super.didAddSubview(subview)
         
-        print("MainView::didAddSubview(_:)")
+        print("MainView::didAddSubview(_:) \(frame)")
     }
     
     override func willRemoveSubview(_ subview: UIView) {
         super.willRemoveSubview(subview)
         
-        print("MainView::willRemoveSubview(_:)")
+        print("MainView::willRemoveSubview(_:) \(frame)")
     }
     
     override func willMove(toSuperview newSuperview: UIView?) {
         super.willMove(toSuperview: newSuperview)
         
-        print("MainView::willMove(toSuperview:)")
+        print("MainView::willMove(toSuperview:) \(frame)")
     }
     
     override func didMoveToSuperview() {
         super.didMoveToSuperview()
         
-        print("MainView::didMoveToSuperview()")
+        print("MainView::didMoveToSuperview() \(frame)")
     }
     
     override func willMove(toWindow newWindow: UIWindow?) {
         super.willMove(toWindow: newWindow)
         
-        print("MainView::willMove(toWindow:)")
+        print("MainView::willMove(toWindow:) \(frame)")
     }
     
     override func didMoveToWindow() {
         super.didMoveToWindow()
         
-        print("MainView::didMoveToWindow()")
+        print("MainView::didMoveToWindow() \(frame)")
     }
     
     // MARK: - Configuring Content Margins
@@ -45,7 +45,7 @@ final class MainView: UIView {
     override func layoutMarginsDidChange() {
         super.layoutMarginsDidChange()
         
-        print("MainView::layoutMarginsDidChange()")
+        print("MainView::layoutMarginsDidChange() \(frame)")
     }
     
     // MARK: - Getting the Safe Area
@@ -53,13 +53,13 @@ final class MainView: UIView {
     override func safeAreaInsetsDidChange() {
         super.safeAreaInsetsDidChange()
         
-        print("MainView::safeAreaInsetsDidChange()")
+        print("MainView::safeAreaInsetsDidChange() \(frame)")
     }
     
     // MARK: - Triggering Auto Layout
     
     override func updateConstraints() {
-        print("MainView::updateConstraints()")
+        print("MainView::updateConstraints() \(frame)")
         
         super.updateConstraints()
     }
@@ -69,7 +69,7 @@ final class MainView: UIView {
     override func layoutSubviews() {
         super.layoutSubviews()
         
-        print("MainView::layoutSubviews()")
+        print("MainView::layoutSubviews() \(frame)")
     }
 }
 
