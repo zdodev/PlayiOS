5731af16584ca9aa8f03c1d5de95eb155df8bf0a
zdodev <iver0945@gmail.com>
2024-04-09 12:35:54 +0900

    ✨: storyboard 제거

--- UIKitTutorial/UIKitTutorial/ViewController.swift
+++ UIKitTutorial/UIKitTutorial/ViewController.swift
@@ -1,14 +1,10 @@
-//
-
 import UIKit
 
-class ViewController: UIViewController {
-
+final class MainViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .systemRed
     }
-
-
 }
 
